f3d3691366ebaa3fbf001136e593389d3ca10c4d
Sam <SamWheating@yahoo.ca>
2019-01-27 08:39:43 -0800

    started to update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,8 +4,16 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <h1>Use this site to view all of your prerequisites!</h1>
-    <p>Select your institution and add classes via our selection dialouge, then press 'Check My Reqs!'' to see an interactive visualization of all your courses.</p>
+    <h1> 
+      ReqCheck makes it <span style="color: #316dc2">easier than ever</span> to visualize course dependencies.
+    </h1>
+    <h2>Instructions:</h2>
+    <h3>Step 1: Select your institution</h3>
+    <h3>Step 2: Add your courses</h3>
+    <h3>Step 3: Press go!</h3>
+    You'll can get more information on each course by hovering your mouse over the corresponding node in the graph.<br><br>
+    <img src="../assets/ExampleGraph.png" alt="Smiley face" border="3">
+    <br>
     <h6>Built with Vue.js</h6>
   </div>
 </template>
