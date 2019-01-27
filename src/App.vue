d3ce6f22ca4cb2c3077d460bc04e3b092de8d546
Sam <SamWheating@yahoo.ca>
2019-01-26 21:49:25 -0800

    made the graphs good

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #848484;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
+<<<<<<< HEAD
     <img alt="Vue logo" src="../assets/req.png">
     <Select/>
+=======
+    <img alt="Vue logo" src="../assets/req.png" style="height:200px; width=100px">
+    <HelloWorld msg="ReqCheck.tech"/>
+>>>>>>> made the graphs good
   </div>
 </template>
 
