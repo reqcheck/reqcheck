0dc98ba987865c1d5801c2d32881f32979e1bdfe
Sam <SamWheating@yahoo.ca>
2019-01-27 04:22:22 -0800

    added select school component

---
+++ src/components/SelectSchool.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <p>First, select your school below:</p>
+        <vue-select ref="select" :options="options" v-model="school"></vue-select>
+  </div>
+</template>
+
+<script>
+
+  import vueSelect from 'vue-select'
+  import { mapActions } from 'vuex'
+  import { mapGetters } from 'vuex'
+ 
+  export default {
+    name: 'SelectSchool',
+    data () {
+        return {
+            school: "",
+        }
+    },
+    computed: {
+        options: function() {
+        let options = ['University of British Columbia', 'University of Victoria']
+        return options
+        },
+        ...mapGetters([
+            'selectedSchool'
+        ])
+    }, 
+    methods: {
+        ...mapActions( {
+            selectSchool: 'selectSchool'
+        } )
+    },
+    watch: {
+        school () {
+            console.log("schoool updated")
+            this.selectSchool(this.school)
+        }
+    },
+    components: {
+        vueSelect
+    },
+  }
+</script>
+
+<style scoped>
+body {
+    font-family: 'Open Sans', sans-serif;
+}
+#app {
+  max-width: 35em;
+  margin: 1em auto;
+}
+</style>
