c886ddc0ceaeab14eaf48609b20aa3926fc719f6
Sam <SamWheating@yahoo.ca>
2019-01-27 16:25:58 -0800

    thicccer logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <img alt="ReqCheck logo" src="../assets/req2.png" style="width:412px">
+    <img alt="ReqCheck logo" src="../assets/req2.png" style="width:80%">
     <Select/>
   </div>
 </template>
